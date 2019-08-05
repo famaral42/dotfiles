035cd0708def11f75e8fce32cf966672e7a5fd36
famaral42 <amaral.felipe@gmail.com>
2019-08-05 16:45:57 -0300

    "Copia" to use rsync more easily

--- bash_alias.sh
+++ bash_alias.sh
@@ -18,3 +18,14 @@ alias top20f='du -a | sort -nr | head -n 20'
 alias pyea='conda activate'
 alias pyed='conda deactivate'
 alias pyel='conda env list'
+
+
+# RSYNC
+# --partial       : Keep partially transferred files
+# --progress      : Show progress during transfer
+# -P              : Equivalent to --partial --progress
+# -r, --recursive : recurse into directories
+# -u, --update    : update only (don't overwrite newer files)
+# -t, --times     : Preserve times
+alias copia='rsync -Prut'
+
